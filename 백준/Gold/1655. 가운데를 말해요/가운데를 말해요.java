88e9524f5620aa29bea21cf864609de4ca6202a5
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2025-03-12 13:42:28 +0900

    [Gold II] Title: 가운데를 말해요, Time: 460 ms, Memory: 37496 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            maxHeap.add(num);
+
+            if (!maxHeap.isEmpty() && !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
+                minHeap.add(maxHeap.poll());
+            }
+
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.add(maxHeap.poll());
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.add(minHeap.poll());
+            }
+
+            bw.write(maxHeap.peek() + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
