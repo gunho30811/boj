672fe9fd22fdfb517767c62e59a91b9fc5a39d31
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2025-03-14 09:45:13 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	long N= Long.parseLong(br.readLine());
+    	int k=1;
+    	while(N>1) {
+    		
+    		N=N-(6*k);
+    		k++;
+    	}
+    	System.out.println(k);
+    	
+    }
+    
+    
+}
