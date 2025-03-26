c24b027040c52d7ad0fca082795e1c5712b1beb5
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2025-03-26 17:16:21 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 696 ms, Memory: 37568 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -8,31 +8,32 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            String word = br.readLine();
-            if (word.length() >= M) {
-                map.put(word, map.getOrDefault(word, 0) + 1);
-            }
+        HashMap<String,Integer> map=new HashMap<>();
+        for(int i=0;i<N;i++) {
+        	String word=(br.readLine());
+        	
+        	if(word.length()>=M) {
+        		map.put(word,map.getOrDefault(word,0)+1);
+        	}
         }
-
-        List<String> list = new ArrayList<>(map.keySet());
-
-        list.sort((a, b) -> {
-            int freqA = map.get(a);
-            int freqB = map.get(b);
-            if (freqA != freqB) return freqB - freqA; // 1. 자주 나오는 순
-            if (a.length() != b.length()) return b.length() - a.length(); // 2. 길이 긴 순
-            return a.compareTo(b); // 3. 사전순
+        
+        List<String> list= new ArrayList<>(map.keySet());
+        
+        list.sort((a,b) -> {
+        	int freqA=map.get(a);
+        	int freqB=map.get(b);
+        	if(freqA!=freqB) return freqB-freqA;
+        	if(a.length()!=b.length()) return b.length()-a.length();
+        	return a.compareTo(b);
+        	
         });
-
         StringBuilder sb = new StringBuilder();
         for (String word : list) {
             sb.append(word).append("\n");
         }
 
         System.out.print(sb);
+
+        
     }
 }
