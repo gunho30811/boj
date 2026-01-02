2c02c517133daa6c0fdb6a8baf47e290a5e00dcb
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2026-01-02 17:17:13 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,19 +1,18 @@
 def solution(my_string):
-    total = 0
     
-    #숫자덩어리 cur
+    #cur은 숫자덩어리
     cur=""
+    #total은 result 할 총합 수
+    total = 0    
     for ch in my_string:
-        
-        if(ord('0')<=ord(ch)<=ord('9')):
+        if(ord('0') <= ord(ch) <= ord('9')):
             cur+=ch
         else:
             if(cur!=""):
                 total+=int(cur)
                 cur=""
-    
+    #마지막 숫자덩어리가 모인경우 ex-> ABC123
     if(cur!=""):
         total+=int(cur)
-                
-    
+
     return total
